Fix moving average computation; save plot, model and data from solved run

reacher.py
@@ -10,9 +10,9 @@ class Trainer:
         self.env = env
         self.last_time = time.time()
 
-    def train(self, n_episodes=10):
+    def train(self, max_episodes=200):
         all_agent_scores = []
-        for i in range(n_episodes):
+        for i in range(max_episodes):
 
             _, states, _ = env.reset()
             scores = np.zeros(self.env.num_agents)
@@ -40,14 +40,16 @@ class Trainer:
             
             all_agent_scores.append(scores)
             t = time.time()
-            print('Episode {} ({:.2f}s) -- Min: {} -- Max: {} -- Mean: {}'.format(i, t - self.last_time, np.min(scores), np.max(scores), np.mean(scores)))
-            self.last_time = t
-            # print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)), end="")
-            # torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
-            # torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
-            # if i_episode % print_every == 0:
-            #     print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))
+            mvg_avg = np.mean(all_agent_scores[-100:])
 
+            print('Episode {} ({:.2f}s) -- Min: {:.2f} -- Max: {:.2f} -- Mean: {:.2f} -- Moving Average: {:.2f}'
+                .format(i, t - self.last_time, np.min(scores), np.max(scores), np.mean(scores), mvg_avg))
+            self.last_time = t
+            if mvg_avg > 30 and len(all_agent_scores) >= 100:
+                break
+
+        # Save model
+        self.agent.save()
         return np.array(all_agent_scores)
 
 class EnvUnityMLAgents:
@@ -83,8 +85,13 @@ class EnvUnityMLAgents:
     def close(self):
         self.env.close()
 
+def moving_averages(values, window=100):
+    return [np.mean(values[:i+1][-window:]) for i, _ in enumerate(values)]
 
 if __name__ == "__main__":
+    MAX_EPISODES = 200
+    SOLVE_SCORE = 30
+
     # Setup
     env = EnvUnityMLAgents("./Reacher_Linux_20_Agents_NoVis/Reacher.x86_64")
     # env = EnvUnityMLAgents("./Reacher_Linux_1_Agent_NoVis/Reacher.x86_64")
@@ -92,15 +99,19 @@ if __name__ == "__main__":
     trainer = Trainer(agent, env)
 
     # Train
-    NUM_EPISODES = 100
-    all_scores = trainer.train(n_episodes=NUM_EPISODES)    
+    all_scores = trainer.train(max_episodes=MAX_EPISODES)
+    np.save('all_scores.npy', all_scores)
     env.close()
 
+    # Plot results
     plt.figure(figsize=(20, 10))
     for agent_idx in range(all_scores.shape[1]):
         plt.plot(all_scores[:, agent_idx])
-    plt.fill_between(x=range(NUM_EPISODES), y1=all_scores.min(axis=1), y2=all_scores.max(axis=1), alpha=0.2)
-    plt.plot(all_scores.mean(axis=1), color='black', linewidth=2)
+    plt.fill_between(x=range(len(all_scores)), y1=all_scores.min(axis=1), y2=all_scores.max(axis=1), alpha=0.2)
+    mvg_avgs = moving_averages(np.mean(all_scores, axis=1))
+    print(mvg_avgs)
+    plt.axhline(y = SOLVE_SCORE, color="red")
+    plt.plot(mvg_avgs, color='black', linewidth=2)
     plt.ylabel("score")
     plt.xlabel("episode")
     plt.savefig("scores.png")